0a7b8b7e278d0bef24ed0bc040904e104eff2292
John Karr <jjkkarr1@live.com>
2016-10-15 19:25:19 -0400

    Added firing method.
    -Added firing method. Currently only fires in the down position.

---
+++ RollingThunder/Assets/DestroyonDelay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyonDelay : MonoBehaviour {
+
+    public float delay;
+	// Use this for initialization
+	void Start ()
+    {
+        Destroy(gameObject, delay);
+	}
+	
+	
+}
---
+++ RollingThunder/Assets/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+    public GameObject bottombullet;
+    Transform firepos;
+	// Use this for initialization
+	void Start ()
+    {
+        firepos = transform.FindChild("firepos");
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            fire();
+        }
+	}
+
+    void fire()
+    {
+        Instantiate(bottombullet, firepos.position, Quaternion.identity);
+    }
+}
